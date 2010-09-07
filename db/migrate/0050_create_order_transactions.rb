2e344a64a0474dcf09a7b5d4f10c691afc5b124e
Sean Grove <sean@chuwe.com>
2010-09-06 20:02:44 -0700

    Paypal backend integration abstraction completed.

--- db/migrate/0050_create_order_transactions.rb
+++ db/migrate/0050_create_order_transactions.rb
@@ -4,7 +4,7 @@ class CreateOrderTransactions < ActiveRecord::Migration
       t.integer :order_id
       t.string :action
       t.integer :amount
-      t.boolean :sucess
+      t.boolean :success
       t.string :authorization
       t.string :message
       t.text :params
