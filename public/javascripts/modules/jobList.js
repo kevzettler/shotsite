6757064199ab011f8d43ca2ea89b1f7396961d4a
kevzettler <kevzettler@gmail.com>
2010-09-06 21:54:14 -0700

    added styles and behavior for new jobList module

---
+++ public/javascripts/modules/jobList.js
@@ -0,0 +1,47 @@
+/*
+* renders a job list 
+*
+*
+*/
+(function($){
+  $.core.modules.jobList = function(){
+    
+    function showClick(e){
+      var $this = $(this);
+      e.preventDefault();
+      $this.text("-" + $this.text().substr(1,$this.text().length));
+      $this.next('ul').fadeIn('fast');
+      $this.unbind('click');
+      $this.click(hideClick, this);
+      return false;
+    }
+    
+    function hideClick(e){
+      var $this = $(this);
+      e.preventDefault();
+      $this.text("+" + $this.text().substr(1,$this.text().length));
+      $this.next('ul').fadeOut('fast');
+      $this.unbind('click');
+      $this.click(showClick, this);
+      return false;
+    }
+    
+    
+    return $.extend(Object.create($.core.modules.broForm()), {            
+      render : function(){
+        var $this = $(this.element),
+        $links = $this.find('.job_link, .batch_link');
+        
+        $links.each(function(){
+          var $this = $(this);
+          $this.text("+"+$this.text());
+          $this.next('ul').hide();
+          $this.click($.proxy(showClick, this));
+        });
+        
+        
+      }
+    
+    });
+  }
+})(jQuery);
