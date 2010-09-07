32bf304cb87adc4a044f71ae801c1c6d5a5d3455
kevzettler <kevzettler@gmail.com>
2010-09-06 20:24:22 -0700

    added helper methods to screenshot model for icons and pretty names

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -10,4 +10,32 @@ class Screenshot < ActiveRecord::Base
   def folder
     "#{self.batch.job.id}_#{self.batch.od}"
   end
+
+	def pretty_name
+		names = {
+			"chrome" => "Fire Fox",
+			"googlechrome" => "Google Chrome",
+			"safari" => "Safari",
+			"iexplore" => "Internet Explorer"
+		}
+		
+		ret = names[self.browser_name]
+		
+		if(self.browser_name == 'iexplore')
+			ret = ret +" " + self.browser_version
+		end
+		
+		ret
+	end
+	
+	def icon_image
+		icons = {
+			"chrome" => "img-firefox.png",
+			"safari" => "img-safari.png",
+			"googlechrome" => "img-chrome.png",
+			"iexplore" => "img-ie8.png",
+			"iexplore" => "img-ie7.png", 
+			"iexplore" => "img-ie6.png"
+		}
+	end
 end
