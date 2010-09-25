007b93965a10f8c47b294adab1f1b999fd2dc7bd
kevzettler <kevzettler@gmail.com>
2010-09-25 12:33:34 -0700

    added show/hide features to the cred control

--- public/javascripts/modules/credControl.js
+++ public/javascripts/modules/credControl.js
@@ -10,13 +10,32 @@
     function show(){
       $(this.element).slideDown();
     }
+    
+    function hide(){
+      $(this.element).slideUp();
+    }
         
     return $.extend(Object.create($.core.module), {
       render : function(){
+        
+        if(window.location.pathname == "/orders"){
+          var $closeBtn = $('<a class="close" title="close">X</a>');
+          $closeBtn.click($.proxy(hide, this));
+          $(this.element).prepend($closeBtn);
+        }
+        
         $(document).bind('show.credControl', $.proxy(show, this));
+        $(document).bind('hide.credControl', $.proxy(hide, this));
         
         $("#new_order_expand").click(function(){
-           $(document).trigger('show.credControl');
+           var $this = $(this);
+           if($this.text() != 'Close Order Form'){
+             $this.text("Close Order Form");
+             $(document).trigger('show.credControl');
+           }else{
+             $this.text("New Order?");
+             $(document).trigger('hide.credControl');
+           }
            return false;
         });
         
